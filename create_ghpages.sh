37d5da14ff429671e4f452024eb67dedd0f605be
Joel Kim <kim.dohhyoung@gmail.com>
2018-01-06 22:24:08 +0900

    Create create_ghpages.sh

---
+++ create_ghpages.sh
@@ -0,0 +1,11 @@
+# current git branch must be `docs-korean`
+git add . -A
+git commit -m "automatic commit"
+git push origin docs-korean
+
+git checkout -B gh-pages
+git rebase docs-korean
+git push -f origin gh-pages
+git reset --hard HEAD
+git clean -f
+git checkout docs-korean
